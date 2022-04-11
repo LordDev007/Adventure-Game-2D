cc1f3b0e81f206d663f32a56a379ee3e0e78122b
LordDev007 <96934124+LordDev007@users.noreply.github.com>
2022-04-11 14:10:25 +0700

    v0.1.1122.220411F
    Fix the issue which the player can go through the door without the key and get stuck there.

--- src/com/Main/CollisionChecker.java
+++ src/com/Main/CollisionChecker.java
@@ -88,19 +88,34 @@ public class CollisionChecker {
                     case "down":
                         entity.solidArea.y += entity.speed;
                         if(entity.solidArea.intersects(gp.obj[i].solidArea)){
-
+                            if(gp.obj[i].collision == true){
+                                entity.collisionOn = true;
+                            }
+                            if(player == true){
+                                index = i;
+                            }
                         }
                         break;
                     case "left":
                         entity.solidArea.x -= entity.speed;
                         if(entity.solidArea.intersects(gp.obj[i].solidArea)){
-
+                            if(gp.obj[i].collision == true){
+                                entity.collisionOn = true;
+                            }
+                            if(player == true){
+                                index = i;
+                            }
                         }
                         break;
                     case "right":
                         entity.solidArea.x += entity.speed;
                         if(entity.solidArea.intersects(gp.obj[i].solidArea)){
-
+                            if(gp.obj[i].collision == true){
+                                entity.collisionOn = true;
+                            }
+                            if(player == true){
+                                index = i;
+                            }
                         }
                         break;
                 }
