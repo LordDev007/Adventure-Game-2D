2f703522d8b7920211a5ea7a0414d58e2c08f6ed
LordDev007 <96934124+LordDev007@users.noreply.github.com>
2022-04-11 13:52:19 +0700

    v0.1.1122.220411
    An update brings interactive to objects.

--- src/com/Main/Main.java
+++ src/com/Main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	    JFrame windows = new JFrame();
         windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         windows.setResizable(false);
-        windows.setTitle("Dungeon Adventure 2D" + " " + "Build 1120" + " " + "\uD83C\uDDE9\uD83C\uDDEA\uD83C\uDDFB\uD83C\uDDEA\uD83C\uDDF1\uD83C\uDDF4\uD83C\uDDF5\uD83C\uDDEA\uD83C\uDDF7");
+        windows.setTitle("Dungeon Adventure 2D" + " " + "Build 1122" + " " + "\uD83C\uDDE9\uD83C\uDDEA\uD83C\uDDFB\uD83C\uDDEA\uD83C\uDDF1\uD83C\uDDF4\uD83C\uDDF5\uD83C\uDDEA\uD83C\uDDF7");
 
         GamePanel gamePanel = new GamePanel();
         windows.add(gamePanel);
@@ -22,15 +22,15 @@ public class Main {
         gamePanel.startGameThread();
     }
 }
-//Build_On_Java_SDK17//version:0.1.1097//BasedOn:FlatEngine2.0+&IntellijIDEA2021.3.3//Dawn.AllCopyrightsReserved
+//Build_On_Java_SDK17//version:0.1.1122//BasedOn:FlatEngine2.0+&IntellijIDEA2021.3.3//Dawn.AllCopyrightsReserved
 //opensourceGame//PleaseReportBugsAt:"hoangbinhminh2508@gmail.com"
 /*
 * allVersionWasCreatedOn: 1021 Legacy
-* previousBuildWas: 1097 ʙᴇᴛᴀ
-* nextBuildWas: 1120 🇩🇪🇻🇪🇱🇴🇵🇪🇷
+* previousBuildWas: 1120 ʙᴇᴛᴀ
+* nextBuildWas: 1146 🇩🇪🇻🇪🇱🇴🇵🇪🇷
 * Zoom IN & OUT was disable because bugs' performance
 * deadlineStartOn:4/4/2022
 * deadlineEndOn:1/5/2022
-* 30% DONE + 0% DLC (May not have)
+* 31% DONE + 0% DLC (May not have)
 * TODO: Create a base game with inventory, stamina and monsters (Build 1263 Early Access)
 */
